cbfa2ecfaac34517271d9239d2e114cc72c603a2
Your Name <you@example.com>
2025-08-02 16:33:16 +0700

    add monitoring

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const client = require('prom-client');
+
 const app = express();
-
 const port = 3000;
-const greeting = process.env.GREETING || 'Hello, World!';
 
+// Set up default metrics collection
+client.collectDefaultMetrics();
+
+// Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+// Root endpoint
+const greeting = process.env.GREETING || 'Hello, World!';
 app.get('/', (req, res) => {
   res.send(greeting);
 });
